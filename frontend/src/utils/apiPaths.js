4f113301cf3a5f4996db688830f33bd6bfc7d774
Ankit Kushwah <ankitkushwah6195@gmail.com>
2025-11-13 12:11:05 +0530

    backend url updated

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://resumexpert-backend.onrender.com";
+export const BASE_URL = "https://resume-builder-backend-5ye0.onrender.com/";
 
 //utils/apiPath.js
 export const API_PATHS = {
