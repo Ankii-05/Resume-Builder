cd2e3a2bc00b6fc88be48bff25a8fa8368853234
Ankit Kushwaha <168106135+MrAnkit7566@users.noreply.github.com>
2026-07-01 23:43:42 +0530

    fix cors errors

--- backend/server.js
+++ backend/server.js
@@ -98,7 +98,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 /* -----------------------------------
    Body Parsers
